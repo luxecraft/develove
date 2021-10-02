7ccb785ab6bcf8e2fe26e539a680f48c56b4bb0e
Harisaran G <hari@luxecraft.org>
2021-10-02 08:05:37 +0530

    add: `AuthState`

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:supabase/supabase.dart';
+import 'package:develove/components/auth_required.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends AuthRequiredState<HomeView> {
+
+  String? _userId;
+
+   @override
+  void onAuthenticated(Session session) {
+    final user = session.user;
+    if (user != null) {
+      _userId = user.id;
+    }
+  }
+
+  @override
+  void onUnauthenticated() {
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,7 +35,26 @@ class HomeView extends StatelessWidget {
             begin: Alignment.topLeft,
             end: Alignment.bottomRight),
       ),
-      child: Scaffold(),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _userId != null ? 'Welcome $_userId' : 'No user',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
