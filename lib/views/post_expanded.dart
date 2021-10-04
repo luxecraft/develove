2c82122a56ad4d00eb49c5698bcb2919d30cc3d2
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-10-04 08:44:03 +0530

    Added Code Snippet

--- lib/views/post_expanded.dart
+++ lib/views/post_expanded.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_highlight/flutter_highlight.dart';
+import 'package:flutter_highlight/themes/atom-one-dark-reasonable.dart';
 
 class PostExpandedView extends StatelessWidget {
   final String title;
   const PostExpandedView({Key? key, required this.title}) : super(key: key);
+
+  final code = """class MyHomePage extends StatefulWidget { 
+        MyHomePage({Key key, this.title}) : super(key: key); 
+        final String title; 
+        
+        @override _MyHomePageState createState() => _MyHomePageState();
+}""";
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +28,35 @@ class PostExpandedView extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               AppBar(),
-              Text(title),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                    vertical: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15.0),
+                        child: HighlightView(
+                          code,
+                          padding: EdgeInsets.all(8.0),
+                          tabSize: 4,
+                          language: 'dart',
+                          theme: atomOneDarkReasonableTheme,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
