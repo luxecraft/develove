a744f0147cd0269fe8cb2426d14df6897a5eeee6
Harisaran G <hari@luxecraft.org>
2021-10-04 14:05:53 +0530

    add: func `updateuser`

---
+++ lib/utils/signup_complete.dart
@@ -0,0 +1,14 @@
+import 'package:develove/utils/constants.dart';
+
+Future<void> updateUser(String fullName, List<String> tags, String? username) async {
+
+  var data = {
+    'username': username ?? await supabase.auth.currentUser!.email,
+    'email': await supabase.auth.currentUser!.email,
+    'fullName': fullName,
+    'tags': tags,
+  };
+  final res = await supabase.from('users').insert(data).execute();
+  print(res.status);
+  print(res.error);
+}
