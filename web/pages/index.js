bb14e665989addef6913ddf2c8c3d18a40e2039c
Harisaran G <hari@luxecraft.org>
2021-10-05 10:51:40 +0530

    add: hooks

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,53 +1,10 @@
-import styles from "../styles/Home.module.css";
-import { createClient } from "@supabase/supabase-js";
-import { useState, useEffect } from "react";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON
-);
+import { AuthProvider } from "../lib/auth";
+import { Dummy } from "../components/dummy";
 
 export default function Home() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const token = localStorage.getItem("supabase.auth.token");
-    console.log(token);
-    if (token) {
-      const refresh_token =
-        JSON.parse(token)["currentSession"]["refresh_token"];
-      console.log(refresh_token);
-      supabase.auth.signIn({
-        refreshToken: refresh_token,
-      });
-    }
-    supabase.auth.onAuthStateChange((event, session) => {
-      console.log(session);
-      setUser(session.user);
-    });
-  }, []);
-
-  async function signUpWithGoogle() {
-    await supabase.auth.signIn({
-      provider: "google",
-    });
-  }
-
-  async function signUpWithGithub() {
-    await supabase.auth.signIn({
-      provider: "github",
-    });
-  }
-
   return (
-    <div className={styles.container}>
-      {user !== null ? null : (
-        <button onClick={() => signUpWithGoogle()}>Sign Up with Google</button>
-      )}
-      {user !== null ? null : (
-        <button onClick={() => signUpWithGithub()}>Sign Up with Github</button>
-      )}
-      <p>{user !== null ? user.email : null}</p>
-    </div>
+    <AuthProvider>
+      <Dummy />
+    </AuthProvider>
   );
 }
