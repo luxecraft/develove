bd537326fc9ba38ee2bb4ed9246e7dfebbfe8a16
Harisaran G <hari@luxecraft.org>
2021-10-05 14:33:27 +0530

    add: accept connection

--- web/lib/connection.js
+++ web/lib/connection.js
@@ -14,3 +14,42 @@ export async function newConnection(tid) {
   console.log(userRes.data);
   console.log(res.data);
 }
+
+export async function acceptConnection(tid) {
+  let currentUser = supabase.auth.user().email;
+  let userRes = await supabase
+    .from("users")
+    .select("uid")
+    .match({ email: currentUser });
+  const data = {
+    fid: userRes.data[0]["uid"],
+    tid: tid,
+  };
+  let res = await supabase
+    .from("connections")
+    .update({ connected: true })
+    .match(data);
+  data = {
+    tid: userRes.data[0]["uid"],
+    fid: tid,
+    connected: true,
+  };
+  res = await supabase.from("connections").insert(data);
+  print(userRes.data);
+  print(res.data);
+}
+
+export async function getConnections() {
+  let currentUser = supabase.auth.user().email;
+  let userRes = await supabase
+    .from("users")
+    .select("uid")
+    .match({ email: currentUser });
+  let res = await supabase
+    .from("connections")
+    .select()
+    .match({ fid: userRes.data[0]["uid"] });
+  return res.data;
+}
+
+export async function pendingConnections() {}
