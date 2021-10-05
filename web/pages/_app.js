4b45f3a3bc0f4067614177ac7474a15a42be9bdb
Sabesh Bharathi <sabeshbharathi19094@ssn.edu.in>
2021-10-05 13:33:21 +0530

    Fix tailwind

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,9 +1,12 @@
 import { AuthProvider } from "../lib/auth";
 import "../styles/globals.css";
+import "../tailwind.css";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Navbar />
       <Component {...pageProps} />
     </AuthProvider>
   );
