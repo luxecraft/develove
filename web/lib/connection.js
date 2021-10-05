340b34d85d028a80c04537dcaffd853d569dd7eb
Sabesh Bharathi <sabeshbharathi19094@ssn.edu.in>
2021-10-05 21:25:49 +0530

    Add explore search user base UI

--- web/lib/connection.js
+++ web/lib/connection.js
@@ -65,7 +65,7 @@ export async function pendingConnections() {
   return res.data;
 }
 
-export async function searchConnections(email) {
+export async function searchUsers(email) {
   let res = await supabase.from("users").select("uid").match({ email: email });
   if (res.data.length == 0) {
     return -1;
--- web/pages/explore.js
+++ web/pages/explore.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ExploreCards from "../components/Explore";
 import SearchBar from "../components/SearchBar";
-export default function explore() {
+import { getUser, searchUsers } from "../lib/connection";
+
+export default function Explore() {
+  const [user, setUser] = useState({});
+  const [error, seterror] = useState(false);
+
+  const searchExplore = async (emailQuery) => {
+    let res = await searchUsers(emailQuery);
+    if (res == -1) {
+      console.log("nope");
+      seterror(true);
+    } else {
+      let res2 = await getUser(res);
+      console.log(res2);
+      setUser(res2);
+    }
+  };
+
   return (
     <div>
-      <SearchBar handleSubmit={(w) => console.log(w)} />
+      <SearchBar handleSubmit={(query) => searchExplore(query)} />
+      <div className="flex items-center justify-center">
+        <ExploreCards data={user} error={error} />
+      </div>
     </div>
   );
 }
