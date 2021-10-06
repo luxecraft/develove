a438b8e0f04ffcf2cf4e3e79fc447e0ceb89a8eb
programVeins <sabesh5601@gmail.com>
2021-10-07 02:33:33 +0530

    Implement signup tweaks

--- web/pages/onboarding.js
+++ web/pages/onboarding.js
@@ -24,26 +24,63 @@ export default function OnBoarding() {
 
   return (
     <div className="block text-center">
+      <h1 className="text-3xl font-bold font-mono text-white mb-10 mt-40">
+        Almost there! Join the awesomest community now! 🔥
+      </h1>
       <form onSubmit={handleSubmit}>
         <div className="my-5">
+          <label className="text-white text-2xl font-thin mr-10">
+            Full Name
+          </label>
           <input
+            className="rounded-lg p-2"
             name="fullName"
             value={userObject.fullName}
             onChange={handleChange}
           />
         </div>
         <div className="my-5">
-          <input name="tags" value={userObject.tags} onChange={handleChange} />
+          <label className="text-white text-2xl font-thin mr-10">
+            Your tags
+          </label>
+          {/* <select
+            className="rounded-lg p-2"
+            name="tags"
+            // value={userObject.tags}
+            onChange={handleChange}
+            multiple
+          >
+            <option className="rounded-lg p-2" value="">
+              Select Tags
+            </option>
+            <option value="javascript">Javascript</option>
+            <option value="react">React</option>
+            <option value="node">Node</option>
+            <option value="express">Express</option>
+          </select> */}
+          <input
+            className="rounded-lg p-2"
+            name="tags"
+            value={userObject.tags}
+            onChange={handleChange}
+          />
         </div>
         <div className="my-5">
+          <label className="text-white text-2xl font-thin mr-10">
+            Username
+          </label>
           <input
+            className="rounded-lg p-2"
             name="username"
             value={userObject.username}
             onChange={handleChange}
           />
         </div>
 
-        <button className=" bg-primary-solid" onSubmit={handleSubmit}>
+        <button
+          className=" bg-primary-solid px-10 py-2 rounded-lg my-10"
+          onSubmit={handleSubmit}
+        >
           Submit
         </button>
       </form>
--- web/pages/signup.js
+++ web/pages/signup.js
@@ -1,39 +1,58 @@
 import { useRouter } from "next/dist/client/router";
 import React, { useEffect } from "react";
 import { useAuth } from "../lib/auth";
+import Image from "next/image";
 
 export default function Signup() {
   const { currentUser, signUpWithGithub, signUpWithGoogle } = useAuth();
   const router = useRouter();
   useEffect(() => {
-    if (currentUser) router.push("/");
+    //Implement logic to check if currentUser exists in supabase, if not, redirect to onboarding
+    if (currentUser) {
+      router.push("/");
+    }
   }, [currentUser, router]);
   return (
     <div className="flex justify-center ">
-      <div className=" h-96 w-5/12 mt-20 bg-accentGray rounded-lg shadow-lg text-center">
-        <h1 className="font-mono font-bold text-2xl mt-5 text-white">
+      <div className=" w-5/12 mt-40 bg-accentGray rounded-lg shadow-lg text-center">
+        <h1 className="font-mono font-bold text-4xl my-10 text-primary-solid">
           Join us at Develove
         </h1>
         <div className="">
           <div>
             <button
               onClick={() => signUpWithGoogle()}
-              className="h-12 w-5/12 bg-gradient-to-r from-primary-start to-primary-end rounded-md my-5 p-2"
+              className="h-12 w-5/12 bg-white rounded-md my-5 p-2"
             >
-              Sign in with Google
+              <div className="flex px-12 items-center">
+                <Image src="/google.png" alt="google" height="25" width="25" />
+                <p className="font-mono text-lg font-medium ml-5">
+                  Sign in with Google
+                </p>
+              </div>
             </button>
           </div>
           <div>
             <button
               onClick={() => signUpWithGithub()}
-              className="h-12 w-5/12 bg-gradient-to-r from-primary-start to-primary-end rounded-md my-5 p-2"
+              className="h-12 w-5/12 bg-white rounded-md my-5 p-2"
             >
-              Sign in with Github
+              <div className="flex px-12 items-center">
+                <Image src="/github.png" alt="github" height="25" width="25" />
+                <p className="font-mono text-lg font-medium ml-5">
+                  Sign in with Github
+                </p>
+              </div>
             </button>
           </div>
           <div>
-            <button className="h-12 w-5/12 bg-gradient-to-r from-primary-start to-primary-end rounded-md my-5 p-2">
-              Sign in with Apple
+            <button className="h-12 w-5/12 bg-white rounded-md my-5 mb-16 p-2">
+              <div className="flex px-12 items-center">
+                <Image src="/apple.png" alt="apple" height="25" width="25" />
+                <p className="font-mono text-lg font-medium ml-5">
+                  Sign in with Apple
+                </p>
+              </div>
             </button>
           </div>
         </div>
