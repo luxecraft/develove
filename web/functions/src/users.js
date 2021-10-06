2dc24d530ac2cd5f3e589cf4991d7f8e85a0c17d
Harisaran G <hari@luxecraft.org>
2021-10-06 10:18:28 +0530

    add: users endpoint

--- web/functions/src/users.js
+++ web/functions/src/users.js
@@ -14,13 +14,27 @@ exports.users = functions.https.onRequest((req, res) => {
     connectionTimeoutSeconds: 2,
   });
 
-  return client
-    .collections()
-    .document()
-    .create()
-    .then((collections) => {
-      functions.logger.info(collections);
-      res.send(collections);
-      res.status(200);
-    });
+  if (req.body.type === "DELETE") {
+    return client
+      .collections("users")
+      .documents(req.body.old_record.uid.toString())
+      .delete()
+      .then((collections) => {
+        functions.logger.info(collections);
+        res.send(collections);
+        res.status(200);
+      });
+  } else {
+    const record = req.body.record;
+    record.id = record.uid.toString();
+    return client
+      .collections("users")
+      .documents()
+      .upsert(record)
+      .then((collections) => {
+        functions.logger.info(collections);
+        res.send(collections);
+        res.status(200);
+      });
+  }
 });
