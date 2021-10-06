4f61f98cc57ab61a2baec6f311bcae88dbe2e2cb
Harisaran G <hari@luxecraft.org>
2021-10-06 12:39:53 +0530

    fix: deploy error

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,3 +1,41 @@
-const { users } = require("./src/users");
+const Typesense = require("typesense");
+const functions = require("firebase-functions");
 
-exports.users = users;
+exports.helloWorld = functions.https.onRequest((req, res) => {
+  const client = new Typesense.Client({
+    nodes: [
+      {
+        host: "develove.ts.luxecraft.org",
+        port: "443",
+        protocol: "https",
+      },
+    ],
+    apiKey: "TPBbQ8qrVfkokxdno0Xt8GakbmH26Wkcjy15VhMPnK3jlNR4",
+    connectionTimeoutSeconds: 120,
+    retryIntervalSeconds: 120,
+  });
+
+  if (req.body.type === "DELETE") {
+    return client
+      .collections("users")
+      .documents(req.body.old_record.uid.toString())
+      .delete()
+      .then((collections) => {
+        functions.logger.info(collections);
+        res.send(collections);
+        res.status(200);
+      });
+  } else {
+    const record = req.body.record;
+    record.id = record.uid.toString();
+    return client
+      .collections("users")
+      .documents()
+      .upsert(record)
+      .then((collections) => {
+        functions.logger.info(collections);
+        res.send(collections);
+        res.status(200);
+      });
+  }
+});
